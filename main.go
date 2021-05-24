2c8aeb39c2d10776f1f6859a35293ff0695a3d63
zhaokai417 <zhaokai417@163.com>
2021-05-24 22:28:17 +0800

    air

--- main.go
+++ main.go
@@ -6,15 +6,24 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello\n")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func hello_world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>hello zk</h1>")
+	fmt.Fprint(w, "<h1>hello zk\n</h1>")
+	fmt.Fprintf(w, "url = %s", r.URL.Path)
 }
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/zk", hello_world)
-	http.ListenAndServe(":7000", nil)
+	http.ListenAndServe(":8040", nil)
 }
