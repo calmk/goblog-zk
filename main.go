d9d6d23773dff9ea11cfb16ee640c3c8ca8f08b4
zhaokai417 <zhaokai417@163.com>
2021-05-25 17:24:31 +0800

    engine

--- main.go
+++ main.go
@@ -5,25 +5,28 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
-func hello_world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>hello zk\n</h1>")
-	fmt.Fprintf(w, "url = %s", r.URL.Path)
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/zk", hello_world)
-	http.ListenAndServe(":8040", nil)
+	router := http.NewServeMux()
+
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	http.ListenAndServe(":8000", router)
 }
