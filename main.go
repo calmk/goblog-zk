15113d928199324c1ba399f87be4bfd87051b4e3
zhaokai <zhaokai417@163.com>
2021-06-24 22:16:38 +0800

    切割模板

--- main.go
+++ main.go
@@ -2,33 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"goblogCalmk/app/http/middlewares"
 	"goblogCalmk/bootstrap"
 	"goblogCalmk/pkg/database"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 var router = mux.NewRouter()
 var db *sql.DB
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 
@@ -38,7 +21,5 @@ func main() {
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":8000", removeTrailingSlash(router))
+	http.ListenAndServe(":8000", middlewares.RemoveTrailingSlash(router))
 }
